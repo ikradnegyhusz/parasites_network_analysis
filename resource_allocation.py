faa1731822bdbad16530a5ead07cbe2cc1e9948c
unknown <andras.fekete106@gmail.com>
2024-11-15 14:13:05 +0100

    completed projections

---
+++ resource_allocation.py
@@ -0,0 +1,38 @@
+import numpy as np
+import networkx as nx
+import pandas as pd
+# row stochastic adjacency matrix, of going from host to parasite
+def average_resource_allocation(badj: np.array) -> np.array:
+    A = badj / badj.sum(axis=1)[:, np.newaxis] # if the row is zero :()
+    A = np.nan_to_num(A, nan=0)
+    At = badj.T / badj.T.sum(axis=1)[:, np.newaxis] # 
+    At = np.nan_to_num(A, nan=0, posinf=0, neginf=0)
+    weights = A @ At
+    np.fill_diagonal(weights, 0)
+    return (weights + weights.T) / 2
+
+
+
+def obtain_bipartite_animals_parasites_graph(df: pd.DataFrame) -> tuple[np.array, list]:
+    """All neighbouring node or edge features are copied to parasites (bipartite=1) """
+    G = nx.Graph()
+
+    for _, el in df.iterrows():
+        if G.has_node(el['Host']):
+            G.add_node(el['Host'], bipartite=0)
+        if G.has_node(el['Parasite']):
+            G.nodes[el['Parasite']]['locality'].add(el['locality'])
+            G.nodes[el['Parasite']]['host_group'].add(el['hostgroup'])
+        else:
+            locality = set()
+            locality.add(el['locality'])
+            host_group = set()
+            host_group.add(el['hostgroup'])
+
+            G.add_node(el['Parasite'], bipartite=1, host_group=host_group, para_group=el['group'], locality=locality)
+        if not G.has_edge(el['Parasite'], el['Host']):
+            G.add_edge(el['Parasite'], el['Host'], )
+
+    bipartite_one_nodes = sorted(n for n, d in G.nodes(data=True) if d.get('bipartite') == 1)
+    badj = nx.bipartite.biadjacency_matrix(G, row_order=bipartite_one_nodes).toarray()
+    return (badj, bipartite_one_nodes)
